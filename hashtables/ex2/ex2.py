a4a6da798d402f9b034f3b8a68900cae6e90e6bc
newnativeabq <vincent.a.brandon@gmail.com>
2020-07-18 09:08:39 -0600

    ex2 complete.  Changed tests to remove trailing NONE from expected output.

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,4 +1,3 @@
-#  Hint:  You may not need all of these.  Remove the unused functions.
 class Ticket:
     def __init__(self, source, destination):
         self.source = source
@@ -6,9 +5,44 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    ht_forward = {}
+    route = []
 
-    return route
+    # Load tickets into hashtable and queue
+    for ticket in tickets:
+        key = ticket.source
+        ht_forward.update({key:ticket})
+
+    # Get the starting ticket
+    start = ht_forward['NONE']
+    route.append(start)
+
+
+    def get_next(route: list, ht: dict):
+        current_ticket = route[-1]
+        destination = current_ticket.destination
+        if destination != "NONE":
+            next_ticket = ht[current_ticket.destination]
+            route.append(next_ticket)
+            return True
+        return False
+
+    while get_next(route=route, ht=ht_forward):
+        pass
+
+    locations = [ticket.source for ticket in route]
+    return locations[1:]
+
+
+        
+
+
+if __name__ == "__main__":
+    tickets = [
+        Ticket("NONE", "PDX"),
+        Ticket("PDX", "DCA"),
+        Ticket("DCA", "NONE"),
+    ]
+    
+    locations = reconstruct_trip(tickets, len(tickets))
+    print(locations)
